5660a81544080eb1de6242977ac423194a42938c
Ramzza <thezniqer@gmail.com>
2022-02-01 18:35:45 +0200

    sort history data

--- iot-vizsga-fe/src/components/ActionHistory.js
+++ iot-vizsga-fe/src/components/ActionHistory.js
@@ -6,13 +6,13 @@ const ActionHistory = (props) => {
 	const [actions, setActions] = useState([]);
 
 	const columns = [
-		{ field: 'id', headerName: 'ID', width: 300 },
-		{ field: 'timestamp', headerName: 'Timestamp', width: 200 },
-		{ field: 'commandType', headerName: 'Key', width: 130 },
+		{ field: 'id', headerName: 'ID', width: 250 },
+		{ field: 'timestamp', headerName: 'Timestamp', width: 300 },
+		{ field: 'commandType', headerName: 'Key', width: 200 },
 		{
 			field: 'commandValue',
 			headerName: 'Value',
-			width: 90,
+			width: 100,
 		},
 	];
 
@@ -22,16 +22,19 @@ const ActionHistory = (props) => {
 		items.forEach((item) => {
 			const key = item.key;
 			const data = item.val();
+			const date = new Date(data.time);
 			actionsNew.push({
 				key: key,
 				id: key,
-				timestamp: data.time,
+				timestamp: date.toLocaleString(),
+				date: date,
 				commandType: data.value.split('=')[0],
 				commandValue: data.value.split('=')[1],
 			});
 		});
 
 		if (actions.length !== actionsNew.length) {
+			actionsNew.sort((a, b) => b.date.getTime() - a.date.getTime());
 			setActions(actionsNew);
 		}
 	};
